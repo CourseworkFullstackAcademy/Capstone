c31415d029240553e17947ac1ffa7045d773858d
DebFullstack <debbieking1979@gmail.com>
2023-09-07 14:33:57 -0400

    draft of cart page, havent checked if rendering

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,24 +1,37 @@
-import { useState, useEffect } from "react";
+import  { useState } from "react";
 import { CartItem } from "./cart-item";
 
 import "./cart.css";
 
-export default function Cart () {
-  const [cartItem, setCartItem ] = useState([]);
+export default function Cart() {
+  const [cartItems, setCartItems] = useState([]);
 
-  
+  // Example function to add an item to the cart
+  const addToCart = (item) => {
+    // Check if the item is already in the cart
+    const existingItemIndex = cartItems.findIndex((cartItem) => cartItem.id === item.id);
+
+    if (existingItemIndex !== -1) {
+      // If the item is already in the cart, update its quantity
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[existingItemIndex].quantity += 1;
+      setCartItems(updatedCartItems);
+    } else {
+      // If the item is not in the cart, add it as a new item
+      setCartItems([...cartItems, { ...item, quantity: 1 }]);
+    }
+  };
+  console.log(addToCart);
 
   return (
-    <div className="cart">    
-        <div className="cart-title"></div>
-        <div className="contianer">
-          <div className="row">
-            <div className="col-3">
-              {}
-            </div>
-          </div>
-        </div>
-    
+    <div className="cart">
+      <div className="cart-title">Cart</div>
+      <div className="container">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 }
+
